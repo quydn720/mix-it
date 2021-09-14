49a234829051ca2efa8f6d228adb2746824f7b7e
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-09-14 23:23:00 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/quydn720/drink_n_chill/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.quydn720.drink_n_chill
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:drink_n_chill/pages/drink_pages/body.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Body(),
+      ),
+      theme: ThemeData.dark(),
+    );
+  }
+}
